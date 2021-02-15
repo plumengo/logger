5d025fee5988fd6e505c2d5445e666a7ada2f2e1
plumengo <72603108+plumengo@users.noreply.github.com>
2021-02-15 20:28:48 +0100

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,4 +1,8 @@
-function logger() {
+const colors = require('colors')
+
+function logger(...optionalParams) {
+    let ts = new Date(Date.now()).toISOString()
+    console.log(ts.gray.bold, ...optionalParams)
 }
 
 module.exports = logger
